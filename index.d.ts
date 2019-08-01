af1c9482cde5fc6bdc5f486024a54cb190c2f786
Phil Tsaryk <phil@tsaryk.com>
2019-08-01 22:05:07 +0200

    Delete internal type definition file
    In favor of @types/mockdate.
    
    Fix: #26

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for mockdate 2.0.2
-// Project: mockdate
-// Definitions by: Bruno Konrad <https://github.com/brunoskonrad>
-//                 Kav Singh <https://github.com/kavsingh>
-
-export = {
-  set(date: Date | string | number, timezoneOffset?: number): void,
-  reset(): void;
-}
