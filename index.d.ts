2736f9da0a699bba176d36978895246c2f5565da
Bob Lauer <bob@lambdaschool.com>
2020-05-20 16:54:16 -0500

    Remove type definition

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-// Type definitions for mockdate 2.0.5
-// Project: mockdate
-// Definitions by: Bruno Konrad <https://github.com/brunoskonrad>
-//                 Kav Singh <https://github.com/kavsingh>
-
-export function set(date: Date | string | number, timezoneOffset?: number): void;
-export function reset(): void;
