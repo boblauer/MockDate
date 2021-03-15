c5d7f995c4d950c5faf3dae02206b922c73671f5
Bob Lauer <bob@lambdaschool.com>
2021-03-15 07:18:44 -0500

    Remove dead code

--- src/mockdate.ts
+++ src/mockdate.ts
@@ -63,10 +63,6 @@ export function set(date: string | number | Date): void {
   // @ts-ignore
   Date = MockDate;
 
-  if (date.valueOf) {
-    date = date.valueOf();
-  }
-
   now = dateObj.valueOf();
 }
 
